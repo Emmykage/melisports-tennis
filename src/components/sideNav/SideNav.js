08ccd201fcf1d1e1566086d1ce76b3270f852cf7
menanya <emmymenz@gmail.com>
2022-11-21 10:59:29 +0100

    fix linters

--- src/components/sideNav/SideNav.js
+++ src/components/sideNav/SideNav.js
@@ -1,32 +1,42 @@
-import React from 'react'
-import './sidenav.css'
+import React from 'react';
+import './sidenav.css';
 
-const SideNav = () => {
-  return (
-    <>
-        <div className='side-row'>
-           <h6>Activities</h6>
+const SideNav = () => (
+  <>
+    <div className="side-row">
+      <h6>Activities</h6>
 
-            <input type="checkbox" id='tennis'/><label for="activity" style={{fontSize:"1rem"}}>Tennis</label>
-        </div>
-        <div className='side-row'>
-            <h6>Racquet Type</h6>
-            <input type="checkbox" id='activity'/>
-            <label for="activity" style={{fontSize:"1rem"}}>Activity</label>
-        </div>
-        <div className='side-row'>
-            <h6>Intermediate</h6>
-            <input type="checkbox" id='activity'/>
-            <label for="activity" style={{fontSize:"1rem"}}>Activity</label>
-        </div>
-        <div className='side-row'>
-            <h6>Brand</h6>
-            <input type="checkbox" id='babolat'/>
-            <label for="activity" style={{fontSize:"1rem"}}>babolat</label>
-        </div>
+      <label htmlFor="activity" style={{ fontSize: '1rem' }}>
+        Tennis
+        <input type="checkbox" id="tennis" />
+      </label>
+    </div>
+    <div className="side-row">
+      <h6>Racquet Type</h6>
 
-    </>
-  )
-}
+      <label htmlFor="activity" style={{ fontSize: '1rem' }}>
+        Activity
+        <input type="checkbox" id="activity" />
+      </label>
+    </div>
+    <div className="side-row">
+      <h6>Intermediate</h6>
 
-export default SideNav
+      <label htmlFor="activity" style={{ fontSize: '1rem' }}>
+        Activity
+        <input type="checkbox" id="activity" />
+      </label>
+    </div>
+    <div className="side-row">
+      <h6>Brand</h6>
+
+      <label htmlFor="activity" style={{ fontSize: '1rem' }}>
+        babolat
+        <input type="checkbox" id="babolat" />
+      </label>
+    </div>
+
+  </>
+);
+
+export default SideNav;
