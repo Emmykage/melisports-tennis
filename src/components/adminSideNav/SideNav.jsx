9e4249a183125b04687357b7b998f436b26a1129
menanya morris <emmiemenz@gmail.com>
2022-12-28 01:36:19 -0800

    admin page initialize

---
+++ src/components/adminSideNav/SideNav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SideNav = () => {
+  return (
+    <aside>
+        
+    </aside>
+
+
+        
+   
+  )
+}
+
+export default SideNav
---
+++ src/pages/admin-page/AdminHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideNav from '../../components/adminSideNav/SideNav'
+
+const AdminHome = () => {
+  return (
+    <SideNav/>
+
+ 
+        
+   
+  )
+}
+
+export default AdminHome
