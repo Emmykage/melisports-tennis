6699618abb310ec59813d344ffad940fb7a6b7d6
morris menanya <emmiemenz@gmail.com>
2023-04-19 10:09:56 +0100

    form client side validation

--- src/pages/auth/AdminSignUp.jsx
+++ src/pages/auth/AdminSignUp.jsx
@@ -48,13 +48,15 @@ const AdminSignUp = () => {
           <h1>Sign Up</h1>
         <form onSubmit={handleSubmit}>
             <label htmlFor="username">username</label>
-            <input type='text' name='username' value={formInput.user.username} onChange={handleInput} placeholder="username" id='username'/>
+            <input type='text' name='username' value={formInput.user.username} onChange={handleInput} placeholder="username" id='username' required/>
             <label htmlFor="email">email</label>
-            <input type={'email'} name="email" value={formInput.user.email} onChange={handleInput} placeholder="email" />
+            <input type={'email'} name="email" value={formInput.user.email} onChange={handleInput} placeholder="email" required/>
             <label htmlFor="mobile">mobile</label>
-            <input type={'text'} name="phone_no" value={formInput.user.phone_no} onChange={handleInput} placeholder="phone no" id='mobile'/>
+            <input type={'text'} name="phone_no" value={formInput.user.phone_no} onChange={handleInput} placeholder="phone no" id='mobile' required/>
             <label htmlFor="password">password</label>
-            <input type="password" value={formInput.user.password} onChange={handleInput} name="password" placeholder='Enter password' />
+            <input type="password" value={formInput.user.password} onChange={handleInput} name="password" placeholder='Enter password' 
+             pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+             title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required/>
             <label htmlFor="role"></label>
             <input type="hidden" name='role' value={formInput.user.client} id='role' />
            
