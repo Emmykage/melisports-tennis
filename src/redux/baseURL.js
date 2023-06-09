fc138cfd34b735ce352d72753ae8f8bcebc44282
morris menanya <emmiemenz@gmail.com>
2023-06-09 10:01:16 +0100

    addd

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -1,6 +1,7 @@
 // const baseURL = 'https://melisports.herokuapp.com/api/v1/';
 // const baseURL = "http://localhost:3000/api/v1/";
 // const baseURL = 'https://melisports.onrender.com/api/v1/';
+
 const baseURL = "http://16.170.250.243:3003/api/v1/"
 
 export default baseURL;
