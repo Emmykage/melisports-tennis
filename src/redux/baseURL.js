b9a744038a66f4b92caaa6b601ba1a28ab27dea2
morris menanya <emmiemenz@gmail.com>
2023-06-15 14:47:38 +0100

    update baseUrl

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -2,6 +2,6 @@
 // const baseURL = "http://localhost:3000/api/v1/";
 // const baseURL = 'https://melisports.onrender.com/api/v1/';
 
-const baseURL = "http://16.16.172.129:3003/api/v1/"
+const baseURL = "http://LB-secured-292027883.eu-north-1.elb.amazonaws.com//api/v1/"
 
 export default baseURL;
