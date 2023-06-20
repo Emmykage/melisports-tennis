7efc9484231f55bd0cde45a06f4510971ae85422
morris menanya <emmiemenz@gmail.com>
2023-06-20 15:00:44 +0100

    change base url

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -4,6 +4,6 @@
 
 // const baseURL = "http://LB-secured-292027883.eu-north-1.elb.amazonaws.com//api/v1/"
 
-const baseURL = "https://melisports.com//api/v1/"
+const baseURL = "https://api.melisports.com//api/v1/"
 
 export default baseURL;
