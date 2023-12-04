f6aa1fc33beb246a0f6e47bb6a58e604a85797f6
morris menanya <emmiemenz@gmail.com>
2023-12-04 03:00:05 +0100

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,27 +1,85 @@
 import React from 'react'
 import Hero from '../components/banner/Hero'
+import AboutBanner from '../components/banner/AboutBanner'
 
 const About = () => {
   return (
     <div className='container about bg-theme text-white'>
-    <Hero/>
+    <AboutBanner/>
   
     <div className='about-us'>
-    <div className='segment grid grid-2 gap-2'>
+    <div className='segment segment-one grid grid-2 gap-2 my-3 py-6'>
         <div className='flex justify-center items-align'>
-          <h2>About Us</h2>
+          <h2 className='text-white font-medium'>About Us</h2>
         </div>
         <div className='flex justify-center items-align p-2'>
-          <p>
-          Meli Business Company Limited (MBCL) is a Nigerian sports equipment and accessories company that brings top-quality products and services from top sports brands such as Babolat. While Melisports is its e-ommerce platform and a one-stop shop for everything sports.
-The Nigeria sport industry still remains largely untapped as it is currently a growing sector, in terms of business and sporting activities. As more public-private partner sponsorship enters into the scene, Melisports will position it self to be the one-stop shop for everything sports.
-Meli Business Company Limited (MBCL) is headquarters in Abuja
+          <p className='w-max-400'>
+          Founded with a passion for sports and a commitment to serving the vibrant community of Nigerian sports enthusiasts, Melisports brings a fresh perspective to the world of sporting goods. Our mission is simple yet profound: to provide access to high-quality sports equipment and services that empower every individual to pursue their athletic goals.
 
           </p>
 
         </div>
 
         </div>
+        
+        <div className='grid grid-2  my-3 py-2'>
+          
+  
+
+          
+          <div className='font-normal'>
+            <ul>
+              <li className='my-3 py-1'>
+                
+                <p> <strong className='spantara font-normal'>Local Expertise, Global Standards: </strong>As a proudly Nigerian company, we understand the unique needs of our community. Our curated selection of sports equipment meets international standards, ensuring that you have access to the best gear without compromise.</p>
+              </li>
+              <li className='py-1 my-2'>
+                <strong>Wide Range of Products:</strong>
+                <p> Whether you're into football, basketball, running, or fitness training, Melisports has you covered. Explore our diverse range of products, carefully selected to cater to athletes of all ages and skill levels.</p>
+              </li>
+              <li className='my-2 py-1'>
+                
+                <p><strong>Affordability and Value:</strong> We believe that everyone should have access to quality sports equipment without breaking the bank. Melisports is committed to offering competitive prices and value for money, making top-notch gear accessible to all.
+</p>
+              </li>
+              <li className='py-1 my-2'>
+                <p><strong>Exceptional Customer Service:</strong> Your satisfaction is our priority. Our knowledgeable and friendly team is here to assist you, providing expert advice to help you make informed decisions about the right equipment for your needs.</p>
+              </li>
+              <li className='py-4 py-3'>
+                <p><strong>Fast and Reliable Delivery:</strong>
+
+                We understand the excitement of getting your hands on new sports gear. That's why we offer fast and reliable delivery services, ensuring that your equipment reaches you in top condition and ready for action.</p>
+              </li>
+            </ul>
+          
+
+ 
+
+
+
+
+
+
+          </div>
+            <div className='flex justify-center items-center'>
+            <h2 className='text-3xl line-space font-medium'>Why Choose Melisports:</h2>
+            </div>
+          <div>
+          
+          </div>
+        </div>
+        <div className='segment segment-three grid grid-2 p-4'>
+          <div>
+            <h1 className='text-4xl'>Join the Melisports Community:</h1>
+          </div>
+            <p>
+              
+
+Beyond being a sports retailer, Melisports is a community of like-minded individuals who share a passion for fitness and an active lifestyle. Connect with us on social media, participate in our events, and be part of a growing network of sports enthusiasts across Nigeria.
+
+Whether you're a professional athlete, a weekend warrior, or someone just starting their fitness journey, Melisports is here to support you every step of the way. Elevate your game, embrace the joy of movement, and experience sports like never before with Melisports—your trusted partner in Nigerian sports excellence!</p>
+          </div>
+                    
               
     </div>
     </div>
