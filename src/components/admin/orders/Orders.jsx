c561d84568e7b4dbc179883b28e46dcd030a9c1f
morris menanya <emmiemenz@gmail.com>
2024-06-20 09:44:31 +0100

    add variance

--- src/components/admin/orders/Orders.jsx
+++ src/components/admin/orders/Orders.jsx
@@ -6,45 +6,45 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { getOrders } from '../../../redux/actions/orders';
 
-// const orders = [{
-//   id: 1,
-//   name: ' Pure drive Lite',
-//   cost: 120000,
-//   quantity: 2,
-//   image: 'https://media.babolat.com/image/upload/f_auto,q_auto,c_pad,w_1008,h_1008/v1651570884/Product_Media/2023/TENNIS_RACKETS/EXPERT/101479-Pure_Aero-370-1-Face.png',
-//   show: false,
-// },
-// {
-//   id: 2,
-//   name: ' Pure Aero Lite',
-//   cost: 120000,
-//   quantity: 2,
-//   image: 'https://media.babolat.com/image/upload/f_auto,q_auto,c_pad,w_1008,h_1008/v1651570884/Product_Media/2023/TENNIS_RACKETS/EXPERT/101479-Pure_Aero-370-1-Face.png',
-//   show: false,
+const orders = [{
+  id: 1,
+  name: ' Pure drive Lite',
+  cost: 120000,
+  quantity: 2,
+  image: 'https://media.babolat.com/image/upload/f_auto,q_auto,c_pad,w_1008,h_1008/v1651570884/Product_Media/2023/TENNIS_RACKETS/EXPERT/101479-Pure_Aero-370-1-Face.png',
+  show: false,
+},
+{
+  id: 2,
+  name: ' Pure Aero Lite',
+  cost: 120000,
+  quantity: 2,
+  image: 'https://media.babolat.com/image/upload/f_auto,q_auto,c_pad,w_1008,h_1008/v1651570884/Product_Media/2023/TENNIS_RACKETS/EXPERT/101479-Pure_Aero-370-1-Face.png',
+  show: false,
 
-// },
-// {
-//   id: 3,
-//   name: ' Evo drive Lite',
-//   cost: 120000,
-//   quantity: 2,
-//   image: 'https://media.babolat.com/image/upload/f_auto,q_auto,c_pad,w_1008,h_1008/v1651570884/Product_Media/2023/TENNIS_RACKETS/EXPERT/101479-Pure_Aero-370-1-Face.png',
-//   show: false,
+},
+{
+  id: 3,
+  name: ' Evo drive Lite',
+  cost: 120000,
+  quantity: 2,
+  image: 'https://media.babolat.com/image/upload/f_auto,q_auto,c_pad,w_1008,h_1008/v1651570884/Product_Media/2023/TENNIS_RACKETS/EXPERT/101479-Pure_Aero-370-1-Face.png',
+  show: false,
 
-// }];
+}];
 
 const Orders = () => {
   const dispatch = useDispatch();
 
   const activeLink = '';
-  const { orders, status } = useSelector((state) => state.orders);
+  // const { orders, status } = useSelector((state) => state.orders);
   const [isDisplayOpen, setIsDisplayOpen] = useState(false);
 
   useEffect(() => {
     dispatch(getOrders());
   }, []);
   return (
-    <div className="order-container">
+    <div className="order-container bg-white p-4 rounded">
       <h1 className="bolder">
         Page Under construction
       </h1>
