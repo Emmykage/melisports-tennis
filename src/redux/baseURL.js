17be6c0d1c78310a3a8eb1fb8dc7a62a40c24e4d
morris menanya <emmiemenz@gmail.com>
2024-07-13 15:49:03 +0100

    update base url

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -1,5 +1,5 @@
-// const baseURL = 'https://melisports-b5453615f37a.herokuapp.com/api/v1/';
-const baseURL = 'http://localhost:3000/api/v1/';
+const baseURL = 'https://melisports-b5453615f37a.herokuapp.com/api/v1/';
+// const baseURL = 'http://localhost:3000/api/v1/';
 // const baseURL = 'https://melisports.onrender.com/api/v1/';
 
 export default baseURL;
