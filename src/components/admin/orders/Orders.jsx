cac19b773534c3679a75a43c5f99ea4ada230fdb
morris menanya <emmiemenz@gmail.com>
2024-11-07 13:16:31 +0100

    update base url

--- src/components/admin/orders/Orders.jsx
+++ src/components/admin/orders/Orders.jsx
@@ -24,27 +24,27 @@ const Orders = () => {
   const [objectId, setObjectId] = useState(null);
   const columnHelper = createColumnHelper();
 
-  const statusStyle = (status) => {
+
+  const statusStyle = (status) =>{
     switch (status) {
-      case 'declined':
-        return 'bg-red-200 text-red-800';
-
-      case 'confirmed':
-        return 'bg-green-200 text-green-800';
-
-      case 'pending':
-        return 'bg-orange-200 text-orange-800';
-
+      case "declined":
+        return "bg-red-200 text-red-800"
+        
+      case "confirmed":
+        return "bg-green-200 text-green-800"
+        
+      case "pending":
+        return "bg-orange-200 text-orange-800"       
+          
       default:
-        return 'bg-orange-200 text-orange-800';
-    }
-  };
+        return "bg-orange-200 text-orange-800"           }
+  }
 
   const handleDelete = (id) => {
     dispatch(deleteOrder(id)).then((result) => {
       if (deleteOrder.fulfilled.match(result)) {
         setOpen(false);
-        dispatch(getOrders());
+        dispatch(getOrders())
         dispatch(toggleAlert({
           isOpen: true,
           message: result.payload.message,
@@ -89,13 +89,7 @@ const Orders = () => {
     }),
 
     columnHelper.accessor('total', {
-      cell: (info) => (
-        <span className="font-semibold">
-          {' '}
-          {nairaFormat(info.getValue())}
-          {' '}
-        </span>
-      ),
+      cell: (info) =>  <span className='font-semibold '> {nairaFormat(info.getValue())} </span> ,
       footer: (props) => props.column.id,
     }),
     columnHelper.accessor('id', {
