1eecf72cb299ef79c443d5e7b2b8fb5137264c77
morris menanya <emmiemenz@gmail.com>
2024-11-11 14:04:50 +0100

    adjust insight

--- src/pages/admin-page/dashBoard/Expenses.jsx
+++ src/pages/admin-page/dashBoard/Expenses.jsx
@@ -15,11 +15,11 @@ const Expenses = () => (
 
       </div>
       <div className="progresser relative w-16 h-16">
-        <svg>
-          <circle  cx="28" cy="28" r="26" />
+        <svg className='w-full h-full'>
+          <circle  cx="28" cy="28" r="24" />
         </svg>
         <div className="number absolute top-0 left-0 w-full h-full flex justify-center items-center">
-          <p>81%</p>
+          <p className='font-semibold '>81%</p>
         </div>
       </div>
 
