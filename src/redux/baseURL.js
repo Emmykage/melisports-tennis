8072f6678e74b61e5d0fb805a6446cfc3910273b
morris menanya <emmiemenz@gmail.com>
2025-07-18 12:52:37 +0100

    enter

--- src/redux/baseURL.js
+++ src/redux/baseURL.js
@@ -1,5 +1,5 @@
 const baseURL = 'https://melisports-b5453615f37a.herokuapp.com/api/v1/';
-//  const baseURL = 'http://localhost:3000/api/v1/'
+//  const baseURL = 'http://localhost:3000/api/v1/';
 // export const publicKey = 'pk_test_9ff n293bee5b397191f0d59ea6bab4ff2bc6e2374';
 
 export const publicKey = 'pk_live_47c6e8e8153d456bf02390af7378e14beb533674';
